4908f814df2244bd2ea46cda97e95c69b0d0aafc
Tom Davis <github@tomdavis.org.uk>
2023-01-24 23:37:40 +0000

    finished

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,32 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+
+        if s.len() == 0 {
+            return Person::default()
+        }
+
+        let s_split: Vec<&str> = s.split(",").collect();
+
+        if s_split.len() != 2 {
+            return Person::default();
+        }
+
+        let name =  s_split.get(0).unwrap().to_string();
+
+        if name.len() == 0 {
+            return Person::default();
+        }
+
+        let age = match s_split.get(1).unwrap().parse::<usize>() {
+            Ok(a) => a,
+            Err(e) => return Person::default(),
+        };
+
+        Person{ name, age }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -28,7 +28,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -46,6 +45,32 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+
+        let s_split: Vec<&str> = s.split(",").collect();
+
+        if s_split.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+
+        let name  = s_split.get(0).unwrap().to_string();
+
+        if name.len() == 0 {
+            return Err(ParsePersonError::NoName);
+        }
+
+        let age;
+
+        match s_split.get(1).unwrap().parse::<usize>() {
+            Ok(a) => age = a,
+            Err(e) => return Err(ParsePersonError::ParseInt(e))
+        }
+
+        Ok(Person{ name, age })
+
     }
 }
 
